8e143479bc61a766e6fd80dcc629976d4c1431f3
ttboma <ttboma@gmail.com>
2024-09-14 22:13:20 +0800

    feat: solution to https://hdlbits.01xz.net/wiki/Lemmings2

---
+++ src/main/scala/hdlbits/circuits/Lemmings2.scala
@@ -0,0 +1,56 @@
+package hdlbits.circuits
+
+import chisel3._
+import chisel3.util._
+
+// _root_ disambiguates from package chisel3.util.circt if user imports chisel3.util._
+import _root_.circt.stage.ChiselStage
+
+// Generate Verilog sources and save it in file Lemmings2.sv
+object Lemmings2 extends App {
+  ChiselStage.emitSystemVerilogFile(
+    new Lemmings2,
+    firtoolOpts = Array(
+      "-disable-all-randomization",
+      "-strip-debug-info"
+    ),
+    args = Array("--target-dir", "gen/hdlbits/circuits")
+  )
+}
+
+// https://hdlbits.01xz.net/wiki/Lemmings2
+class Lemmings2 extends RawModule {
+  val clk = IO(Input(Clock()))
+  val areset = IO(
+    Input(AsyncReset())
+  ) // Freshly brainwashed Lemmings walk left.
+  val bump_left = IO(Input(Bool()))
+  val bump_right = IO(Input(Bool()))
+  val ground = IO(Input(Bool()))
+  val walk_left = IO(Output(Bool()))
+  val walk_right = IO(Output(Bool()))
+  val aaah = IO(Output(Bool()))
+
+  // State registers with clock and reset
+  val walkDirection = withClockAndReset(clk, areset) { RegInit(false.B) }
+  val groundState = withClockAndReset(clk, areset) { RegInit(true.B) }
+
+  // State transition logic
+  groundState := ground
+
+  when(!ground || !groundState) {}
+    .elsewhen(bump_left & bump_right) {
+      walkDirection := ~walkDirection
+    }
+    .elsewhen(bump_right) {
+      walkDirection := false.B
+    }
+    .elsewhen(bump_left) {
+      walkDirection := true.B
+    }
+
+  // Output logic
+  walk_left := Mux(groundState, ~walkDirection, false.B)
+  walk_right := Mux(groundState, walkDirection, false.B)
+  aaah := ~groundState
+}
